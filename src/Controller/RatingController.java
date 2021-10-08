f1336d6c6695afc4a031a9b02f60b94b30f52193
Lee-Hyoeun <gydms0136@gmail.com>
2021-10-08 14:47:28 +0900

    write code and structure method in RatingController

--- src/Controller/RatingController.java
+++ src/Controller/RatingController.java
@@ -1,5 +1,32 @@
 package Controller;
 
+import java.util.ArrayList;
+
+import model.RatingDTO;
+
 public class RatingController {
+    
+    private ArrayList<RatingDTO> list;
+    private int nextId;
+    
+    public RatingController() {
+        list = new ArrayList<>();
+        nextId = 1;
+    }
+    
+    
+    // 평점을 list에 추가하는 메소드
+    
+    //특정 영화의 평점 리스트를 리턴하는 메소드
+    
+    //특정 영화의 평점을 모두 삭제하는 메소드
+    
+    //특정 작성자의 평점을 모두 삭제하는 메소드
+    
+    
+    // 개별보기. 수정. 없음!
+    // 삭제는 특정영화를 두가지 상황
+    // 삭제하면 평점도 같이 사라질때. 특정작성자가 탈퇴해서 작성했던 평점도 같이 사라질때
+    
 
 }
